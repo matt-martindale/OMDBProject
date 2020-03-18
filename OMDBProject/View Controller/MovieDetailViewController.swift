76ec96cb744f52ee48e958c051644913609ad8d3
Matt Martindale <martindalematt18@yahoo.com>
2020-03-17 22:54:57 -0600

    detailVC working

--- OMDBProject/View Controller/MovieDetailViewController.swift
+++ OMDBProject/View Controller/MovieDetailViewController.swift
@@ -18,33 +18,43 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var actorLabel: UILabel!
     @IBOutlet weak var metaScoreLabel: UILabel!
     @IBOutlet weak var plotTextView: UITextView!
+    @IBOutlet weak var posterImage: UIImageView!
     
     var movieDetail = MovieDetail()
     var imdbID: String?
-    var movie: Movie? {
-        didSet {
-            updateView()
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let imdb = imdbID else { return }
         movieDetail.fetchMovieInfo(imdbID: imdb) {
-            
+            DispatchQueue.main.async {
+                self.updateView()
+            }
         }
     }
     
     func updateView() {
-        titleLabel.text = movie?.title
-        yearLabel.text = movie?.year
-        ratedLabel.text = movie?.rated
-        runTimeLabel.text = movie?.runTime
-        directorLabel.text = movie?.director
-        actorLabel.text = movie?.actors
-        metaScoreLabel.text = movie?.metaScore
-        plotTextView.text = movie?.plot
+        titleLabel.text = movieDetail.movie?.title
+        yearLabel.text = movieDetail.movie?.year
+        ratedLabel.text = movieDetail.movie?.rated
+        runTimeLabel.text = movieDetail.movie?.runTime
+        directorLabel.text = movieDetail.movie?.director
+        actorLabel.text = movieDetail.movie?.actors
+        metaScoreLabel.text = "\(movieDetail.movie?.metaScore ?? "-")/100"
+        plotTextView.text = movieDetail.movie?.plot
+        getPosterImage()
     }
     
+    func getPosterImage() {
+        guard let poster = movieDetail.movie?.poster else { return }
+        if let url = URL(string: poster) {
+            do {
+                let data = try Data(contentsOf: url)
+                self.posterImage.image = UIImage(data: data)
+            } catch let err {
+                print("Error loading poster image: \(err.localizedDescription)")
+            }
+        }
+    }
 
 }
